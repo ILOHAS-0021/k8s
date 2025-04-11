c1c78ef950d234a873fa2c1ac2b810adda98406a
ILOHAS-0021 <123717624+ILOHAS-0021@users.noreply.github.com>
2025-04-11 09:37:23 +0900

    Update setup-k8s.sh

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -34,8 +34,7 @@ sed -i '/ swap / s/^/#/' /etc/fstab
 
 # Docker repository 추가
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update -y
 
 # containerd 설치 및 설정
@@ -48,10 +47,8 @@ systemctl enable containerd
 # Kubernetes repository 설정
 apt-get install -y apt-transport-https ca-certificates curl
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key \
-| gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' \
-| tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update -y
 
 # Kubernetes 설치
