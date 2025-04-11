9651a4c8cd9c58c487e979dd54a6d65738c70cc5
ILOHAS-0021 <ilohas0021@naver.com>
2025-04-11 01:10:58 +0000

    update file format

--- master-k8s.sh
+++ master-k8s.sh
@@ -5,10 +5,14 @@ set -e
 kubeadm config images pull --cri-socket unix:///run/containerd/containerd.sock --kubernetes-version v1.30.3
 
 # kubeadm init 실행 및 토큰 추출
-KUBEADM_OUTPUT=$(kubeadm init --pod-network-cidr=10.244.0.0/16 --upload-certs --kubernetes-version=v1.30.3 --cri-socket unix:///run/containerd/containerd.sock --ignore-preflight-errors=all)
+KUBEADM_OUTPUT=$(kubeadm init \
+--pod-network-cidr=10.244.0.0/16 \
+--upload-certs --kubernetes-version=v1.30.3 \
+--cri-socket unix:///run/containerd/containerd.sock \
+--ignore-preflight-errors=all)
 
 # 토큰 정보 추출 및 저장
-echo "$KUBEADM_OUTPUT" | grep -A 2 "kubeadm join" > k8s-token.txt
+echo "$KUBEADM_OUTPUT" | grep -A 2 "kubeadm join" > token.txt
 
 # kubeconfig 설정
 mkdir -p $HOME/.kube
@@ -17,3 +21,4 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Flannel CNI 적용
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+
